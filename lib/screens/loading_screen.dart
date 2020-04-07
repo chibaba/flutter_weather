42bdd642cc49346885c083b5dced75e878a9108c
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-07 16:38:53 +0100

    networking with http request api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -16,14 +17,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
   void getLocation() async {
-    Position position = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location ();
+   await location.getCurrentLocation();
+   print(location.latitude);
+   print(location.longitude);
 
+
+  }
+
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?'
+        'id=2172797&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
   @override
   Widget build(BuildContext context) {
-
-
+    getData();
     return Scaffold();
 
   }
